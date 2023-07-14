754fe3d945bcf4bddb320c0e7d88295b46d3a536
shinkuan <shinkuan318@gmail.com>
2023-07-15 00:56:40 +0800

    Can iterate through all 512 pages.

--- B_ICC2012_preliminary_all_cell/NFC.v
+++ B_ICC2012_preliminary_all_cell/NFC.v
@@ -25,22 +25,19 @@ reg        F_IO_A_READING;
 
 reg [3:0] state;
 reg [8:0] page;     //Total of 512 pages, 9 bit.
-reg [1:0] state_address;
-reg       state_command;
-
-
+reg [8:0] counter;  //Count 512.
 
 //FSM States
-localparam STATE_READ_COMMAND = 4'd0;
-localparam STATE_READ_ADDRESS = 4'd1;
-localparam STATE_READ_READING = 4'd2;
+localparam STATE_READ_COMMAND_0 = 4'd0;
+localparam STATE_READ_COMMAND_1 = 4'd1;
+localparam STATE_READ_ADDRESS_0 = 4'd2;
+localparam STATE_READ_ADDRESS_1 = 4'd3;
+localparam STATE_READ_ADDRESS_2 = 4'd4;
+localparam STATE_READ_ADDRESS_3 = 4'd5;
+localparam STATE_READ_ADDRESS_4 = 4'd6;
+localparam STATE_READ_ADDRESS_5 = 4'd7;
+localparam STATE_READ_READING_0 = 4'd8;
 
-localparam STATE_READ_COMMAND_0 = 1'b0;
-localparam STATE_READ_COMMAND_1 = 1'b1;
-
-localparam STATE_READ_ADDRESS_0 = 2'd0;
-localparam STATE_READ_ADDRESS_1 = 2'd1;
-localparam STATE_READ_ADDRESS_2 = 2'd2;
 
 assign F_IO_A = F_IO_A_READING ? 8'hZZ : F_IO_A_OUT;
 assign F_IO_A = F_IO_A_IN;
@@ -49,72 +46,100 @@ always @(posedge clk) begin
     if (rst) begin
         F_IO_A_READING <= 0;
 
-        state         <= STATE_READ_COMMAND;
-        state_address <= STATE_READ_ADDRESS_0;
-        state_command <= STATE_READ_COMMAND_0;
+        state   <= STATE_READ_COMMAND_0;
         page    <= 0;
+        counter <= 0;
 
         F_CLE_A <= 1;
-        F_WEN_A <= 1;
+        F_WEN_A <= 0;
         F_ALE_A <= 0;
         F_REN_A <= 1;
     end else begin
         case (state)
-            STATE_READ_COMMAND: begin
-                // F_CLE_A <= 1;
-                // F_ALE_A <= 0;
-                if (F_WEN_A) begin
-                    F_WEN_A <= 0;
-                    case (state_command)
-                        STATE_READ_COMMAND_0: begin
-                            state_command <= STATE_READ_COMMAND_1;
-                        end
-                        STATE_READ_COMMAND_1: begin
-                            state_command <= STATE_READ_COMMAND_0;
-                            state <= STATE_READ_ADDRESS;
-                            F_CLE_A <= 0;
-                            F_ALE_A <= 1;
-                        end
-                        default: begin
-                            
-                        end 
-                    endcase
-                end else begin
-                    F_WEN_A <= 1;
-                end
-            end
-            STATE_READ_ADDRESS: begin
-                if (F_WEN_A) begin
-                    F_WEN_A <= 0;
-                    case (state_address)
-                        STATE_READ_ADDRESS_0: begin
-                            state_address <= STATE_READ_ADDRESS_1;
-                        end
-                        STATE_READ_ADDRESS_1: begin
-                            state_address <= STATE_READ_ADDRESS_2;
-                        end
-                        default: begin
-                            
-                        end
-                    endcase
-                end else begin
-                    F_WEN_A <= 1;
-                    if (state_address == STATE_READ_ADDRESS_2) begin
-                        state <= STATE_READ_READING;
-                        state_address <= STATE_READ_ADDRESS_0;
-                    end else begin
-                        
-                    end
-                end
-            end
-            STATE_READ_READING: begin
-                F_IO_A_READING <= 1;
+            STATE_READ_COMMAND_0: begin
+                F_CLE_A <= 1;
+                F_WEN_A <= 1;
                 F_ALE_A <= 0;
+                F_REN_A <= 1;
+
+                state <= STATE_READ_COMMAND_1;
+            end
+            STATE_READ_COMMAND_1: begin
                 F_CLE_A <= 0;
+                F_WEN_A <= 0;
+                F_ALE_A <= 1;
+                // F_REN_A <= 1;
+
+                state <= STATE_READ_ADDRESS_0;
+            end
+            STATE_READ_ADDRESS_0: begin
+                // F_CLE_A <= 0;
+                F_WEN_A <= 1;
+                // F_ALE_A <= 1;
+                // F_REN_A <= 1;
+
+                state <= STATE_READ_ADDRESS_1;
+            end
+            STATE_READ_ADDRESS_1: begin
+                // F_CLE_A <= 0;
+                F_WEN_A <= 0;
+                // F_ALE_A <= 1;
+                // F_REN_A <= 1;
+
+                state <= STATE_READ_ADDRESS_2;
+            end
+            STATE_READ_ADDRESS_2: begin
+                // F_CLE_A <= 0;
+                F_WEN_A <= 1;
+                // F_ALE_A <= 1;
+                // F_REN_A <= 1;
+
+                state <= STATE_READ_ADDRESS_3;
+            end
+            STATE_READ_ADDRESS_3: begin
+                // F_CLE_A <= 0;
+                F_WEN_A <= 0;
+                // F_ALE_A <= 1;
+                // F_REN_A <= 1;
+
+                state <= STATE_READ_ADDRESS_4;
+            end
+            STATE_READ_ADDRESS_4: begin
+                // F_CLE_A <= 0;
+                F_WEN_A <= 1;
+                // F_ALE_A <= 1;
+                // F_REN_A <= 1;
+
+                state <= STATE_READ_ADDRESS_5;
+            end
+            STATE_READ_ADDRESS_5: begin
+                // F_CLE_A <= 0;
+                // F_WEN_A <= 1;
+                F_ALE_A <= 0;
+                // F_REN_A <= 1;
+                
+                state <= STATE_READ_READING_0;
+                F_IO_A_READING <= 1;
+            end
+            STATE_READ_READING_0: begin
+                // F_CLE_A <= 0;
+                // F_WEN_A <= 1;
+                // F_ALE_A <= 0;
                 if (F_RB_A && F_REN_A) begin
                     F_REN_A <= 0;
                 end else begin
                     F_REN_A <= 1;
+                    counter <= counter + 1;
+                    if (counter == 9'd511) begin
+                        F_CLE_A <= 1;
+                        F_WEN_A <= 0;
+                        // F_ALE_A <= 0;
+                        state <= STATE_READ_COMMAND_0;
+                        F_IO_A_READING <= 0;
+                        page <= page + 1;
+                    end else begin
+                        
+                    end
                 end
             end
             default: begin
@@ -126,34 +151,29 @@ end
 
 
 always @(*) begin
-    case (state)
-        STATE_READ_COMMAND: begin
-            F_IO_A_OUT = 8'h00; //Here we use h00 instead of h01 is because we want to start at first half page.
-        end
-        STATE_READ_ADDRESS: begin
-            case (state_address)
-                STATE_READ_ADDRESS_0: begin
-                    F_IO_A_OUT = 8'h00;
-                end
-                STATE_READ_ADDRESS_1: begin
-                    F_IO_A_OUT = page[7:0];
-                end
-                STATE_READ_ADDRESS_2: begin
-                    F_IO_A_OUT = {7'b0, page[8]};
-                end
-                default: begin
-                    F_IO_A_OUT = 8'hXX;
-                end
-            endcase
-        end
-        STATE_READ_READING: begin
-            F_IO_A_OUT = {7'b0, page[8]};
-        end
-        default: begin
-            F_IO_A_OUT = 8'hXX;
-        end
-    endcase    
+    if (rst) begin
+        F_IO_A_OUT = 8'h00; //Here we use h00 instead of h01 is because we want to start at first half page.
+    end else begin
+        case (state[3:1])
+            STATE_READ_COMMAND_0[3:1]: begin
+                F_IO_A_OUT = 8'h00;
+            end
+            STATE_READ_ADDRESS_0[3:1]: begin
+                F_IO_A_OUT = 8'h00;
+            end
+            STATE_READ_ADDRESS_2[3:1]: begin
+                F_IO_A_OUT = page[7:0];
+            end
+            STATE_READ_ADDRESS_4[3:1]: begin
+                F_IO_A_OUT = {7'b0, page[8]};
+            end
+            STATE_READ_READING_0[3:1]: begin
+                F_IO_A_OUT = 8'h00;
+            end
+            default: begin
+                F_IO_A_OUT = 8'hXX;
+            end
+        endcase   
+    end  
 end
-
-
 endmodule
