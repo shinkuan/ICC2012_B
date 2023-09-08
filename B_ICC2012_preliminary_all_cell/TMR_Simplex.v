4218962760f045cda844a940b7a3f70e36a5ef75
shinkuan <shinkuan318@gmail.com>
2023-09-09 02:01:37 +0800

    就這樣？

--- B_ICC2012_preliminary_all_cell/TMR_Simplex.v
+++ B_ICC2012_preliminary_all_cell/TMR_Simplex.v
@@ -9,7 +9,7 @@ input [DATA_LEN-1:0]dataA_in,dataB_in,dataC_in;
 input A_error_ctrl,B_error_ctrl,C_error_ctrl;
 input clk,reset;
 
-output reg [DATA_LEN-1:0]data_out;
+output [DATA_LEN-1:0]data_out;
 output reg TMR_error;
 
 wire [DATA_LEN-1:0]_A,_B,_C;
@@ -23,60 +23,23 @@ assign _C=(C_error_ctrl)? ~dataC_in : dataC_in;
 
 assign simplex_mode=A_fault|B_fault|C_fault;
 
-always@(*)
-begin
-	if(simplex_mode)
-		if(A_fault)
-		begin
-			data_out=_B;
-			TMR_error=(_B!=_C);
-		end
-		else if(B_fault)
-		begin
-			data_out=_C;
-			TMR_error=(_A!=_C);
-		end
-		else
-		begin
-			data_out=_A;
-			TMR_error=(_A!=_B);
-		end
-	else
-		if((_A!=_B)&&(_A!=_C)&&(_B!=_C))
-		begin
-			data_out=(_A&_B)|(_B&_C)|(_A&_C);
-			TMR_error=1'b1;
-		end
-		else if((_A!=_B)&&(_A!=_C))
-		begin
-			data_out=(_A&_B)|(_B&_C)|(_A&_C);
-			TMR_error=1'b0;
-		end
-		else if((_A!=_C)&&(_B!=_C))
-		begin
-			data_out=(_A&_B)|(_B&_C)|(_A&_C);
-			TMR_error=1'b0;
-		end
-		else 
-		begin
-			data_out=(_A&_B)|(_B&_C)|(_A&_C);
-			TMR_error=1'b0;
-		end
-end
+assign data_out=(_A&_B)|(_B&_C)|(_A&_C);
 
 always@(posedge clk or posedge reset)
 begin
 	if(reset)
 		begin
-			A_fault<=1'b0;
-			B_fault<=1'b0;
-			C_fault<=1'b0;
+			A_fault   <= 1'b0;
+			B_fault   <= 1'b0;
+			C_fault   <= 1'b0;
+			TMR_error <= 1'b0;
 		end
 	else
 		begin
-			A_fault<=((_A!=_B)&&(_A!=_C))?1'b1:A_fault;
-			B_fault<=((_A!=_B)&&(_B!=_C))?1'b1:B_fault;
-			C_fault<=((_C!=_B)&&(_A!=_C))?1'b1:C_fault;
+			A_fault   <= ((_A!=_B)&&(_A != _C)) ? 1'b1 : A_fault;
+			B_fault   <= ((_A!=_B)&&(_B != _C)) ? 1'b1 : B_fault;
+			C_fault   <= ((_C!=_B)&&(_A != _C)) ? 1'b1 : C_fault;
+			TMR_error <= A_fault&(_B!=_C)|B_fault&(_A!=_C)|C_fault&(_A!=_B);
 		end
 end
 
