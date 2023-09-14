7089b2527cf14d8e1d0b993e03b2d84bf65d44c7
shinkuan <shinkuan318@gmail.com>
2023-09-15 00:20:53 +0800

    WIP Design_CHIP.v

---
+++ B_ICC2012_preliminary_all_cell/Report_U18/Design_CHIP.v
@@ -0,0 +1,20 @@
+module Design_CHIP (
+    I_clk, 
+    I_rst, 
+    O_done, 
+    IO_F_IO_A, 
+    O_F_CLE_A, 
+    O_F_ALE_A, 
+    O_F_REN_A, 
+    O_F_WEN_A, 
+    I_F_RB_A, 
+    IO_F_IO_B, 
+    O_F_CLE_B, 
+    O_F_ALE_B, 
+    O_F_REN_B, 
+    O_F_WEN_B, 
+    I_F_RB_B, 
+    I_KEY
+);
+    
+endmodule
