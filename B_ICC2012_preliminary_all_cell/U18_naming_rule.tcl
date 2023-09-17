acc2bdc9264554f69a62dff4784ee1598e837b14
shinkuan <shinkuan318@gmail.com>
2023-09-17 08:54:35 +0800

    Naming rule tcl

---
+++ B_ICC2012_preliminary_all_cell/U18_naming_rule.tcl
@@ -0,0 +1,11 @@
+remove_unconnected_ports -blast_buses [get_cells * -hier]
+
+set bus_inference_style {%s[%d]}
+set bus_naming_style {%s[%d]}
+set hdlout_internal_busses true
+change_names -hierarchy -rule verilog
+define_name_rules name_rule -allowed "a-z A-Z 0-9 _" -max_length 255 -type cell
+define_name_rules name_rule -allowed "a-z A-Z 0-9 _[]" -max_length 255 -type net
+define_name_rules name_rule -map {{"\\*cell\\*" "cell"}}
+define_name_rules name_rule -case_insensitive
+change_names -hierarchy -rules name_rule
